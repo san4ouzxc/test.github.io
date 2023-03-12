924b64dd9a240a321846395a80001c93be95c107
san4ouzxc <120376962+san4ouzxc@users.noreply.github.com>
2023-03-12 11:51:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log("1488")
+const myButton = document.getElementById("myButton");
+const myText = document.getElementById("myText");
+
+myButton.addEventListener("click", function() {
+  myText.textContent = "Привет, мир!";
+});
